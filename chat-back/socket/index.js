856b1bc80e461dc1b233daf4132303be7ce67db6
“Valdery <valdery.jur@gmail.com>
2021-08-05 19:42:41 -0300

    Adds socket config and events listeners
    Signed-off-by: “Valdery <valdery.jur@gmail.com>

---
+++ chat-back/socket/index.js
@@ -0,0 +1,39 @@
+const ioLib = require('socket.io');
+const { saveMsgOnDB } = require('../services');
+let io;
+
+const configure = server => {
+    io = ioLib(server, {
+        cors: {
+            origin: ["http://localhost:3000"],
+            methods: ["GET", "POST"]
+        }
+    });
+    return io;
+};
+
+/**
+ * @description Listtens for io connection and trigger events
+ */
+const connect = () => {
+    io.on('connection', function(socket){
+        console.log('a user connected');
+        socket.on('chat message', function(msg){
+            console.log('message: ' + msg);
+            // Se eu quisesse salvar o chat no db, posso chamar uma função aqui dentro
+            const savedMsg = saveMsgOnDB(msg);
+            console.log('saved message: ' + savedMsg);
+            // Emite para os usuários do io
+            io.emit('chat message', msg);
+        });
+
+        socket.on('disconnect', function(){
+            console.log('user disconnected');
+        });
+    });
+}
+
+module.exports = {
+    connect,
+    configure,
+}
