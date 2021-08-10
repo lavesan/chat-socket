80d25f27e345fe5808a64ce216b636d745fb94c0
“Valdery <valdery.jur@gmail.com>
2021-08-10 13:05:38 -0300

    Uses the 'main' tag to load the chat
    Signed-off-by: “Valdery <valdery.jur@gmail.com>

--- chat-front/src/components/organisms/ChatPage.jsx
+++ chat-front/src/components/organisms/ChatPage.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
 import Sidebar from '../molecules/Sidebar';
+import Chat from '../molecules/Chat';
 
-const ChatPage = () => (
-  <Grid container>
-    <Sidebar />
-  </Grid>
-);
+const useStyles = makeStyles({
+  content: {
+    flexGrow: 1,
+  },
+});
+
+const ChatPage = () => {
+  const classes = useStyles();
+
+  return (
+    <Grid container>
+      <Sidebar />
+      <main className={classes.content}>
+        <Chat />
+      </main>
+    </Grid>
+  );
+};
 
 export default ChatPage;
