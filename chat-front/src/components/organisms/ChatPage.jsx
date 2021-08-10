7ea0f6a45a407347d9f470aecc1278ccd2291de0
“Valdery <valdery.jur@gmail.com>
2021-08-10 03:07:32 -0300

    Adds the chat page
    Signed-off-by: “Valdery <valdery.jur@gmail.com>

---
+++ chat-front/src/components/organisms/ChatPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import Sidebar from '../molecules/Sidebar';
+
+const ChatPage = () => (
+  <Grid container>
+    <Sidebar />
+  </Grid>
+);
+
+export default ChatPage;
