a8d977f93fa36ef21cd0155e10b283622bec4c5b
“Valdery <valdery.jur@gmail.com>
2021-08-10 03:03:28 -0300

    Adds material-ui theme
    Signed-off-by: “Valdery <valdery.jur@gmail.com>

---
+++ chat-front/src/theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from '@material-ui/core/styles';
+
+export default createTheme({
+  palette: {
+    primary: {
+      main: '#00c700',
+    },
+    secondary: {
+      main: '#d9d9d9',
+    },
+  },
+});
