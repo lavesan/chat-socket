b3e3b868dedb74007248074028ba28f717111866
“Valdery <valdery.jur@gmail.com>
2021-08-10 14:56:29 -0300

    Adds logic to add/get all chat logs
    Signed-off-by: “Valdery <valdery.jur@gmail.com>

--- chat-back/services/index.js
+++ chat-back/services/index.js
@@ -1,7 +1,44 @@
+const fs = require('fs');
+var path = require('path');
+
+const path_file = path.join(__dirname, '..', 'db.json');
+const encoding = 'utf8';
+
 module.exports = {
-    saveMsgOnDB: body => {
-        return {
-            body,
-        }
-    }
+    saveMsgOnDB: async body => {
+        const msgs = await new Promise((success, reject) => {
+            fs.readFile(path_file, encoding, (err, data) => {
+                if (err) reject(err);
+                success(JSON.parse(data));
+            });
+        });
+
+        msgs.push(body);
+
+        return await new Promise((success, reject) => {
+            fs.writeFile(path_file, JSON.stringify(msgs), encoding, err => {
+                if (err) {
+                  reject(err);
+                }
+                success(body);
+            });
+        });
+    },
+    getMsgs: async () => {
+        const msgs = await new Promise((success, reject) => {
+            fs.readFile(path_file, encoding, (err, data) => {
+                if (err) reject(err);
+                success(JSON.parse(data));
+            });
+        });
+        const groups = [];
+        msgs.forEach(msg => {
+            if (!groups.includes(msg.group)) groups.push(msg.group);
+        });
+        const res = {};
+        groups.forEach(group => {
+            res[group] = msgs.filter(msg => msg.group === group);
+        });
+        return res;
+    },
 }
