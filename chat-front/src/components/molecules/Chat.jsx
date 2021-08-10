e34856d11b4d9a00ad09807e308afe4b86c028ac
“Valdery <valdery.jur@gmail.com>
2021-08-10 13:05:02 -0300

    Adds the SendInput on the Chat
    Signed-off-by: “Valdery <valdery.jur@gmail.com>

--- chat-front/src/components/molecules/Chat.jsx
+++ chat-front/src/components/molecules/Chat.jsx
@@ -1,24 +1,66 @@
 import React, { useContext, useMemo } from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, Divider } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
 import AppContext from '../../App.context';
 import Message from '../atoms/Message';
+import SendInput from '../atoms/SendInput';
+
+const useStyles = makeStyles({
+  container: {
+    height: '100vh',
+  },
+  messagesMap: {
+    overflowY: 'scroll',
+    padding: '0 8px',
+    flexGrow: 1,
+  },
+  messageContainer: {
+    marginTop: 8,
+  },
+});
 
 const Chat = () => {
+  const classes = useStyles();
+
   const { selectedGroup, msgs } = useContext(AppContext);
 
   const chatMsgs = useMemo(
-    () => {
-      console.log('chat selected: ', selectedGroup);
-      console.log('chat msgs: ', msgs);
-      console.log('chat msgs: ', msgs[selectedGroup]);
-      return msgs[selectedGroup] || [];
-    },
+    () => msgs[selectedGroup] || [],
     [msgs, selectedGroup],
   );
 
+  const noGroupSelected = useMemo(
+    () => Boolean(selectedGroup),
+    [selectedGroup],
+  );
+
   return (
-    <Grid container>
-      {chatMsgs.map((msg) => <Message {...msg} />)}
+    <Grid
+      container
+      direction="column"
+      classes={{ root: classes.container }}
+      justifyContent="space-between"
+    >
+      <Grid
+        item
+        container
+        direction="column"
+        classes={{ root: classes.messagesMap }}
+      >
+        {chatMsgs.map((msg) => <Message className={classes.messageContainer} {...msg} />)}
+      </Grid>
+      <Grid
+        item
+        container
+        direction="column"
+      >
+        {noGroupSelected && (
+          <>
+            <Divider />
+            <SendInput />
+          </>
+        )}
+      </Grid>
     </Grid>
   );
 };
